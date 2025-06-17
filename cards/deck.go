60533824ac8cbd644a7ed3f611a97a7794c882c7
Brian Njenga <bkuriakb@gmail.com>
2025-06-17 13:18:27 +0300

    feat(decks): add deal function to deal cards

--- cards/deck.go
+++ cards/deck.go
@@ -6,6 +6,23 @@ import "fmt"
 // which a slice of strings
 type deck []string
 
+
+func newDeck() deck {
+	cards := deck{}
+
+	cardSuits := []string{"Spades", "Flowers", "Aces", "Diamonds", "Hearts", "Clubs"}
+
+	cardValues := []string{"Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Jack", "Queen", "King", "Ace"}
+
+	for _, suit := range cardSuits {
+		for _, value := range cardValues { // the underscore is used to ignore the index and only use the value
+			// append to the slice
+			cards = append(cards, suit+" of "+value)
+		}
+	}
+	return cards
+}
+
 // Receiver function
 func (d deck) print() {
 	for i, card := range d {
